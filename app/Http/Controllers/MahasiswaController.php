2c7f650eacc97803f4c840ff6f2f4a1c675d7dfa
unknown <gilangfauzan1@yahoo.com>
2019-12-07 14:48:14 +0700

    video 2 model

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Student;
 
 class MahasiswaController extends Controller
 {
@@ -14,8 +15,7 @@ class MahasiswaController extends Controller
      */
     public function index()
     {
-        $mahasiswa = DB::table('mahasiswa')->get();
-        dump($mahasiswa);
+        $mahasiswa = Student::all();
         return view('mahasiswa.index', ['mahasiswa' => $mahasiswa]);
     }
 
